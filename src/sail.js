5dc4c8224ec1b6538dfe4c349243f838651ed9da
billytrend <me@billytrend.com>
2014-06-02 13:24:55 +0100

    Changed sail angle handling.

--- src/sail.js
+++ src/sail.js
@@ -31,7 +31,7 @@ var sailLoad = new Promise(function(resolve, reject) {
 	sailImage.onload = function() {
 		boat.sail = new kinetic.Image({
 			image: sailImage,
-			offsetX: 10
+			offsetY: 10
 		});
 		resolve(boat.sail);
 	};
@@ -45,10 +45,18 @@ Promise.all([sailLoad, hullLoad]).then(function(response) {
 	boat.add(response[0]);
 	boatsLayer.add(boat);
 	sailingArea.add(boatsLayer);
-	// boat.animateBoat(100, 10);
-	boat.setBoat(100);
-	Promise.all([boat.goForward()]);
+	boat.setSail(135);
+	Promise.all([boat.animateSail(60, -1)]);
 });
+
+var testNormaliseAngle = function() {
+	var i = -12345;
+	while(normaliseAngle(i) < 360 && normaliseAngle(i) >= 0) {
+		i++;
+		if(i == 12345) break;
+	}
+	console.log(i);
+};
 
 var normaliseAngle = function(deg) {
 	if(deg < 0) deg +=360;
@@ -57,6 +65,10 @@ var normaliseAngle = function(deg) {
 
 var toRad = function(deg) {
 	return deg * Math.PI/180;
+};
+
+boat.getAbsoluteRotation = function() {
+	return normaliseAngle(this.rotation());
 };
 
 boat.setSail = function(deg) {
@@ -71,15 +83,15 @@ boat.setBoat = function(deg) {
 };
 
 boat.flipSail = function() {
-	this.sail.scale({ x: this.sail.scale().x * -1 });
+	this.sail.scale({ y: this.sail.scale().y * -1 });
 	this.parent.draw();
 };
 
 boat.checkSailSide = function() {
 	var rot = this.sail.rotation();
-	var scale = this.sail.scale().x;
+	var scale = this.sail.scale().y;
 	if(rot < 90 && rot >= 0 && scale == -1) this.flipSail();
-	if(rot > -90 && rot < 0 && scale == 1) this.flipSail();
+	if(rot <=180 && rot > 90 && scale == 1) this.flipSail();
 };
 
 boat.animateSail = function(deg, speed) {
@@ -88,7 +100,7 @@ boat.animateSail = function(deg, speed) {
 			var absolutePos = this.sail.rotation() + deg;
 			this.sail.curAnimation = new kinetic.Animation(function(frame) {
 				var curRotation = this.sail.rotation();
-				if(curRotation == absolutePos || curRotation >= 90 || curRotation <= -90) {
+				if(curRotation == absolutePos || curRotation >= 180 || curRotation <= 0) {
 					this.sail.curAnimation.stop();
 					resolve();
 				} else {
@@ -107,6 +119,7 @@ boat.animateBoat = function(deg, speed) {
 			var absolutePos = this.rotation() + deg;
 			this.curAnimation = new kinetic.Animation(function(frame) {
 				var curRotation = this.rotation();
+				console.log(physics.getOptimumSailAngle(this));
 				if(curRotation == absolutePos) {
 					this.curAnimation.stop();
 					resolve();
@@ -149,4 +162,35 @@ boat.animateMove = function(deg, distance) {
 boat.goForward = function(){
 	var rot = this.rotation();
 	return this.animateMove(rot, 100);
+};
+
+boat.moveToCoord = function(x, y, steps) {
+	var dX = x/steps,
+		dY = y/steps;
+	return new Promise(function(resolve, reject){
+		var travelled = 0;
+		if(this.movingAnimation !== undefined && this.movingAnimation.isRunning()) {
+			this.movingAnimation.stop();
+		}
+
+		this.movingAnimation = new kinetic.Animation(function(frame){
+			this.setRelPos(dX * ++travelled, dY * travelled);
+			if(travelled > 100) {
+				this.movingAnimation.stop();
+				resolve();
+			}
+		}.bind(this));
+
+		this.movingAnimation.start();
+	}.bind(this));
+};
+
+var physics = {};
+
+physics.getOptimumSailAngle = function(boat) {
+	var rot = normaliseAngle(boat.rotation());
+	if(rot < 180 && rot > 0) {
+		return rot/2;
+	}
+	return -rot/2;
 };
